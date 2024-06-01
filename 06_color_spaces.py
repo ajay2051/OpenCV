51c83c448212764b57c76b27490b8731af759bb5
Ajay Thakur <ajaythk.94@gmail.com>
2024-06-01 13:04:21 +0545

    color_spaces

---
+++ 06_color_spaces.py
@@ -0,0 +1,32 @@
+# OpenCV reads images in BGR Format ie blue, green and red.
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = cv.imread("Photos/cute_krishna.jpg")
+cv.imshow("Cute Krishna", img)
+
+# BGR to GrayScale
+gary = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gary", gary)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow("HSV", hsv)
+
+# BGR to LAB
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow("LAB", lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow("RGB", rgb)
+
+plt.imshow(rgb)
+plt.show()
+
+# HSV to BGR
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow("HSV --> BGR", hsv_bgr)
+
+cv.waitKey(0)
